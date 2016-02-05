148a9cd4a4bc3c67cdf81b54cf6560750d00030f
root <admin@local.host>
2016-02-05 14:05:59 +0800

    selenium

---
+++ src/com/inspur/Hello.java
@@ -0,0 +1,35 @@
+package com.inspur;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.baidu.com");
+		
+		driver.manage().window().maximize();
+		
+		WebElement txtbox = driver.findElement(By.name("wd"));
+		txtbox.sendKeys("Gelln");
+		txtbox.submit();
+		
+		WebElement btn = driver.findElement(By.id("su"));
+		btn.click();
+		
+		driver.close();
+		
+		System.out.println("OK");
+	}
+
+}
